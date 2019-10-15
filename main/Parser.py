86a43d4f418a3a581fc6ed9f6ba9fbfb45875227
Yu Nan <nanyu201411@gmail.com>
2019-10-15 22:33:22 +0800

    [complete] raw code complete

--- main/Parser.py
+++ main/Parser.py
@@ -1,8 +1,57 @@
 class ArgsParser(object):
+    def __init__(self):
+        self.args_dict = {}
+        self._schema = None
+
+    @property
+    def schema(self):
+        return self._schema
+
+    @schema.setter
+    def schema(self, schema_as_text):
+        if not isinstance(schema_as_text, str):
+            raise TypeError("expected a string")
+        self._schema = Schema(schema_as_text)
+
+    def _check_arg_type(self, arg):
+        if self.schema is None:
+            raise ValueError("please set schema")
+        if len(arg) == 1:
+            arg_name = arg
+            arg_type = self.schema.get_type(arg)
+            val = ''
+        else:
+            arg_name, val = arg.split(" ")
+            arg_type = self.schema.get_type(arg_name)
+        if arg_type == "bool" and val == '':
+            return arg_type, arg_name, True
+        try:
+            arg_val = eval(arg_type)(val)
+        except ValueError:
+            raise TypeError("argument {} should be {}".format(arg_name, arg_type))
+        return arg_type, arg_name, arg_val
+
+    def get_args(self, arg):
+        return self.args_dict[arg]
+
     def parse(self, args_as_text):
-        pass
+        args_as_list = args_as_text.split("-")
+        args_as_list.remove("")
+        for arg in args_as_list:
+            print(arg)
+            arg_type, arg_name, arg_val = self._check_arg_type(arg.strip())
+            self.args_dict[arg_name] = arg_val
 
 
 class Schema(object):
     def __init__(self, schema_as_text):
-        pass
+        schema = schema_as_text.split(" ")
+        self.schema_dict = {}
+        for i in schema:
+            key, val = i.split(":")
+            self.schema_dict[key] = val
+
+    def get_type(self, arg):
+        if arg not in self.schema_dict.keys():
+            raise ValueError("unexcept argument %d" % arg)
+        return self.schema_dict[arg]
--- tests/test_args.py
+++ tests/test_args.py
@@ -3,19 +3,32 @@ from main.Parser import ArgsParser, Schema
 
 
 class MyTestCase(unittest.TestCase):
-    def test_parse_text_to_dict(self):
-        # TODO: parse input
-        args_as_text = "-l -p 8080 -d /usr/logs"
-        args_as_dict = ArgsParser().parse(args_as_text)
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.args_parser = ArgsParser()
+        cls.args_parser.schema = "l:bool p:int d:str"
 
-    def test_dict_type(self):
+    def test_set_schema(self):
         # TODO: check args type
-        schema_as_text = "l:boolean p:integer d:string"
+        schema_as_text = "l:bool p:int d:str"
         schema = Schema(schema_as_text)
+        self.assertEqual("bool", schema.get_type("l"))
+        self.assertEqual("int", schema.get_type("p"))
+        self.assertEqual("str", schema.get_type("d"))
+
+    def test_args_type(self):
+        # TODO: check args type
+        self.assertEqual(("bool", "l", True), self.args_parser._check_arg_type("l"))
+        self.assertEqual(("int", "p", 8080), self.args_parser._check_arg_type("p 8080"))
+        self.assertEqual(("str", "d", "/usr/logs"), self.args_parser._check_arg_type("d /usr/logs"))
 
     def test_get_args_value(self):
         # TODO: check value
-        self.assertEqual(True, False)
+        args_as_text = "-l -p 8080 -d /usr/logs"
+        self.args_parser.parse(args_as_text)
+        self.assertEqual(True, self.args_parser.get_args("l"))
+        self.assertEqual(8080, self.args_parser.get_args("p"))
+        self.assertEqual("/usr/logs", self.args_parser.get_args("d"))
 
 
 if __name__ == '__main__':
